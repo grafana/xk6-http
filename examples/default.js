a83ad1805f6a90f3ade1aa472327deaf63bee709
saniyar-dev <saniyar.dev@gmail.com>
2025-03-13 01:22:21 +0330

    [ FIX ] default.js example

--- examples/default.js
+++ examples/default.js
@@ -3,6 +3,7 @@ import { Client } from 'k6/x/net/http';
 export default async function () {
   const client = new Client();
   const response = await client.get('https://httpbin.test.k6.io/get');
-  const jsonData = await response.json();
-  console.log(jsonData);
+  // This behavior will be in next PR:
+  // const jsonData = await response.json();
+  console.log(response);
 }
