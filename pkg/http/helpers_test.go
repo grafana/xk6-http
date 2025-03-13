e4f1f6cdcebd857a9a2babf5e679c6bfb8143cc6
saniyar-dev <saniyar.dev@gmail.com>
2025-03-13 20:45:00 +0330

    [ ADD ] unit tests for dynamicRead function

---
+++ pkg/http/helpers_test.go
@@ -0,0 +1,62 @@
+package http
+
+import (
+	"bytes"
+	"crypto/rand"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func generateLongContent() ([]byte, error) {
+	nBig, err := rand.Int(rand.Reader, big.NewInt(1e6))
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, nBig.Int64())
+	_, err = rand.Read(buf)
+	return buf, err
+}
+
+func TestDynamicRead(t *testing.T) {
+	type testCase struct {
+		readFn  func([]byte) (int, error)
+		timeout time.Duration
+
+		expectedLen int
+		expectedRes []byte
+		error       error
+	}
+
+	t.Run("Read long and random len content", func(t *testing.T) {
+		tests := []testCase{}
+		for range 3 {
+			content, err := generateLongContent()
+			// TODO: maby this is wrong and should be handled another way?
+			if err != nil {
+				assert.Error(t, err)
+			}
+			reader := bytes.NewReader(content)
+
+			tests = append(tests, testCase{
+				readFn:  reader.Read,
+				timeout: time.Second * 1,
+
+				expectedRes: content,
+				expectedLen: reader.Len(),
+			})
+		}
+
+		for _, test := range tests {
+			actualLen, actualRes, err := dynamicRead(test.readFn, test.timeout)
+			t.Logf("The actual content len is %d", actualLen)
+
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedLen, actualLen)
+			assert.Equal(t, test.expectedRes, actualRes)
+		}
+	})
+}
