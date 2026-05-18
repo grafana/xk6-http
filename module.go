b160968970b4c3708fbcfabe18d90344fb3cf336
Mihail Stoykov <312246+mstoykov@users.noreply.github.com>
2026-05-18 18:07:46 +0300

    chore: SHA-pin the k6-ci shared workflow (#24)

--- module.go
+++ module.go
@@ -45,5 +45,5 @@ func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
 // Exports implements the [modules.Instance] interface and returns the exports
 // of the JS module.
 func (mi *ModuleInstance) Exports() modules.Exports {
-	return modules.Exports{Named: map[string]interface{}{}}
+	return modules.Exports{Named: map[string]any{}}
 }
